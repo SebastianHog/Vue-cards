4ac779844d3ca2d960f457664f1dd51c2f8ff653
Sebastian <seblhog@gmail.com>
2024-07-31 11:58:01 +0200

    Logout button works

---
+++ src/Components/LogoutButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    @click="logoutUser"
+    class="w-16 h-8 bg-transparent border-2 border-black rounded-lg"
+  >
+    Log out
+  </button>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    async logoutUser() {
+      try {
+        const resp = await axios.post(
+          "http://localhost:3000/auth/logout",
+          {},
+          { withCredentials: true }
+        );
+        console.log("resp:", resp.status);
+      } catch (e) {
+        console.log("Error:", e);
+      }
+    },
+  },
+};
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="h-screen bg-gray-400">
     <h1>You are logged in</h1>
+    <LogoutButton />
   </div>
 </template>
 
 <script>
 import authenticateUser from "../utils/authenticateUser.ts";
 import { useRouter } from "vue-router";
+import LogoutButton from "../Components/LogoutButton.vue";
 
 export default {
+  components: { LogoutButton: LogoutButton },
   data() {
     return {
       userLoggedIn: false,
